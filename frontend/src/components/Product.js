056d898843cd3bbdd242be0a44c6269899145489
EVATRONIX\dcaputa <dominik.caputa@evatronix.com>
2022-02-13 00:40:16 +0100

    first commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+/* REACT-BOOTSTRAP */
+import { Card } from "react-bootstrap";
+
+/* REACT ROUTER */
+import { Link } from "react-router-dom";
+
+/* COMPONENTS */
+import Rating from "./Rating";
+
+
+function Product({ product }) {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={"https://hidden-meadow-61629.herokuapp.com"+product.image} />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            color={"#f8e825"}
+          />
+        </Card.Text>
+
+        <Card.Text as="h3">{product.price}PLN</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
